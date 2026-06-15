a5b7ccdb1bb8b3e0503119813e847728d00ccd12
Mihir Patel <mk04patel@gmail.com>
2026-06-15 00:35:53 -0500

    Added Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+const Card = (props) => {
+    const [flipped, setFlipped] = useState(false);
+    
+    const handleFlip = () => {
+        setFlipped(!flipped);
+    };
+
+    return (
+        <div id="Card" onClick={handleFlip} className={flipped ? "flipped" : ""}>
+            <div className="card-face front">
+                <h4>Question:</h4>
+                <p>{props.question}</p>
+            </div>
+            <div className="card-face back">
+                <h4>Answer:</h4>
+                <p>{props.answer}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
